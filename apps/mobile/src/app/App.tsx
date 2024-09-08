7dea11cbacbd41454b6b9f5af3d4f5a90fff9eca
daylonblakely <daylonblakely@gmail.com>
2024-09-08 16:16:25 -0700

    expo mobile

---
+++ apps/mobile/src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <TouchableOpacity>
+        <Text>hahahah</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default App;
