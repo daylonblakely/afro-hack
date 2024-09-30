f663d9f00858dfa6c4d9dc803328a39fb9db3c02
daylonblakely <daylonblakely@gmail.com>
2024-09-29 20:59:54 -0700

    comment out firebase

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,26 +2,26 @@ import dotenv from 'dotenv';
 
 // Load .env file
 dotenv.config();
-import * as functions from 'firebase-functions';
+// import * as functions from 'firebase-functions';
 import mongoose from 'mongoose';
 import admin from 'firebase-admin';
 import app from './app';
 
 const port = process.env.PORT || 5000;
 
-// admin.initializeApp({ credential: admin.credential.applicationDefault() });
+admin.initializeApp({ credential: admin.credential.applicationDefault() });
 
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => {
-//     console.log('Connected to MongoDB');
-//   })
-//   .catch((err) => {
-//     console.error('Failed to connect to MongoDB', err);
-//   });
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+  });
 
-// app.listen(port, () => {
-//   console.log(`Server is running at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
 
-export const api = functions.https.onRequest(app);
+// export const api = functions.https.onRequest(app);
